ead8fbb014734bc9f29ecdec551bfbf44f7ca232
pjduimstra <duimstpj@miamioh.edu>
2023-02-01 11:27:19 -0500

    Completed basic functionality of login modal component

--- src/Components/UI/Login.js
+++ src/Components/UI/Login.js
@@ -1,46 +1,48 @@
 import { Card, Button, Form, Row, Modal } from "react-bootstrap";
 import { useForm } from "react-hook-form";
-import "../../Assets/CSS/FeedbackBox.css";
+import "../../Assets/CSS/Login.css";
+
 
 const LoginBox = ({ setOverlayLogin, handleCloseOverlay }) => {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = (data) => {
-        console.log(data);
-        setOverlayLogin(false);
-    };
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    setOverlayLogin(false);
+  };
 
-    return (
-        <Modal show={setOverlayLogin} onHide={setOverlayLogin} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>Login Form</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control 
-              type="email" 
-              placeholder="Enter email" 
-              {...register("email")}/>
-            </Form.Group>
+  return (
+    <Modal show={setOverlayLogin} onHide={() => setOverlayLogin(false)} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>Login to Wheelshare</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Username or Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              {...register("email")} />
+          </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control 
-              type="password" 
-              placeholder="Password" 
-              {...register("password")}/>
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setOverlayLogin(false)}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleSubmit(onSubmit)}>Login</Button>
-        </Modal.Footer>
-      </Modal>
-    );
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              {...register("password")} />
+          </Form.Group>
+        </Form>
+        <Button variant="primary" onClick={handleSubmit(onSubmit)}>Login</Button>
+      </Modal.Body>
+      <Modal.Footer>
+        <div>
+          Don't have an account? Sign-up here:
+          <Button variant="secondary" onClick={handleSubmit(onSubmit)}>Signup</Button>
+        </div>
+      </Modal.Footer>
+    </Modal>
+  );
 };
 
 export default LoginBox;
