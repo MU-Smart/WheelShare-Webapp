4459aaf877122a6eb4566beb22d1befa6af74570
Kian Metcalfe <kian@metcalfe.net>
2023-02-28 14:29:29 -0500

    URL based state

--- src/index.js
+++ src/index.js
@@ -14,6 +14,19 @@ function Root() {
   // TODO - Add routing display
   // TODO - Add data to url paramenters
 
+  const updateURLCoords = (lat, lng) => {
+    const params = new URLSearchParams(window.location.search);
+    params.set('lat', lat);
+    params.set('lng', lng);
+    window.history.pushState(null, '', `?${params.toString()}`);
+  };
+
+  const updateURLZoom = (zoom) => {
+    const params = new URLSearchParams(window.location.search);
+    params.set('zoom', zoom);
+    window.history.pushState(null, '', `?${params.toString()}`);
+  };
+
   return (
     <JSAPILoader
       apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
@@ -22,10 +35,30 @@ function Root() {
       <GoogleMap
         height='100vh'
         width='100vw'
-        center={{ lat: 39.51, lng: -84.73 }}
+        center={{
+          lat:
+            Number(new URLSearchParams(window.location.search).get('lat')) ||
+            39.51,
+          lng:
+            Number(new URLSearchParams(window.location.search).get('lng')) ||
+            -84.73,
+        }}
+        zoom={
+          Number(new URLSearchParams(window.location.search).get('zoom')) || 20
+        }
         options={{
-          zoom: 20,
           disableDefaultUI: true,
+        }}
+        onLoad={(map) => {
+          map.addListener('center_changed', () => {
+            updateURLCoords(
+              map.getCenter().lat().toFixed(4),
+              map.getCenter().lng().toFixed(4)
+            );
+          });
+          map.addListener('zoom_changed', () => {
+            updateURLZoom(map.getZoom());
+          });
         }}
       >
         <ControlUI />
