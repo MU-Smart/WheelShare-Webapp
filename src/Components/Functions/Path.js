acfe4734a972de383a0c097d3ea70303e9065b82
pjduimstra <duimstpj@miamioh.edu>
2023-03-14 15:29:26 -0400

    Added documentation to Path.js

--- src/Components/Functions/Path.js
+++ src/Components/Functions/Path.js
@@ -1,30 +1,45 @@
-const testURL = 'http://mypathweb.csi.miamioh.edu:8081/testRoute';
-
+const testURL = '/api/testRoute';
 
 /**
- * Function to get the route information from a given url with an API endpoint
+ * Function to get the route information from a given url with an API endpoint.
+ * It is important to note that in the development environment, a proxy must be 
+ * used to satisfy the CORS restrictions present on the backend server. The production
+ * code will need to have updated urls to access the data from, as the proxy will not be used.
  * 
- * @param {string} url the API endpoint that the path data is being retrieved from.
+ * @param {string} url the url of the API endpoint that the path data is being retrieved from.
  * 
- * @returns {promise<Object>} path This is an array that contains the array of lat/lng coordinates
+ * 
+ * @returns {promise<Object>} An array that contains the array of lat/lng coordinates
  *                  for MapPath to read as well as the path information to determine the 
  *                  path color based on average uncomfort score.
+ * @example
+ * <MapPath path={getPath('API Endpoint URL')}></MapPath>
  */
 export async function getPath(url) {
+  // Array to hold the coordinates that the path follows.
   let pathData = [];
+
+  // Array to hold the information about the path (avg/max/total uncomfort score)
   let pathInfo = [];
-  
-  return await fetch(url)
+
+  await fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      pathData.push(data.averageUncomfortScore);
-      pathData.push(data.maxUncomfortScore);
-      pathData.push(data.totalUncomfortScore);
+      pathInfo.push(data.averageUncomfortScore);
+      pathInfo.push(data.maxUncomfortScore);
+      pathInfo.push(data.totalUncomfortScore);
+
+      // Add all coordinates from the nodes recieved from the API endpoint
       for (let i = 0; i < data.nodeCount; i++) {
-        pathInfo.push({lat :  data.nodeList[i].latitute, lng :  data.nodeList[i].longtitude});
+        pathData.push({ lat: data.nodeList[i].latitute, lng: data.nodeList[i].longtitude });
       }
-      let path = [pathData, pathInfo];
-      console.log(path[1]);
+
+      // Update return object
+      data = [pathData, pathInfo];
+
+      // Return cleaned JSON data
+      return data;
+      // console.log(data[0]);
     })
     .catch(error => {
       console.error(error);
